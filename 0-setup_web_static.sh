67764dce596d51d66bf2de425c3559e2e18e4acb
eminentetim <etimeminent@gmail.com>
2024-02-01 03:26:27 -0800

    Write a Fabric script that generates a .tgz archive from the contents of the web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,16 +1,4 @@
 #!/usr/bin/env bash
-#A script that sets up a web server for deployment of web_static
-sudo apt-get update
-sudo apt-get -y intall nginx
-sudo ufw allow 'Nginx HTTP'
-
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-sudo touch -p /data/web_static/releases/test/index.html
-#!/usr/bin/env bash
 # script that sets up web servers for the deployment of web_static
 sudo apt-get update
 sudo apt-get -y install nginx
